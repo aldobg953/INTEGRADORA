d6fed743dd7746f8a119df3ccc70de908b28eb83
Aldo Barrera <aldobg953@gmail.com>
2024-07-17 19:28:01 -0600

    Update font family and text styles for configuracion.html and tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,35 +13,55 @@ module.exports = {
     extend: {
       colors: {
         blue: {
-          100: '#E6EDFF', // Variante clara
-          200: '#B3CBFF', // Variante clara
-          DEFAULT: '#0047FF', // Color principal
-          600: '#0038CC', // Variante oscura
-          700: '#002999', // Variante oscura
+          100: '#E6EDFF',
+          200: '#B3CBFF',
+          DEFAULT: '#0047FF',
+          600: '#0038CC',
+          700: '#002999',
         },
         purple: {
-          100: '#F2F1FF', // Variante clara
-          200: '#C7C5FF', // Variante clara
-          DEFAULT: '#9590FF', // Color principal
-          600: '#7773CC', // Variante oscura
-          700: '#5A5799', // Variante oscura
+          100: '#F2F1FF',
+          200: '#C7C5FF',
+          DEFAULT: '#9590FF',
+          600: '#7773CC',
+          700: '#5A5799',
         },
         dark: {
-          100: '#5A5872', // Variante clara
-          200: '#3B394D', // Variante clara
-          DEFAULT: '#1C1A27', // Color principal
-          600: '#16151F', // Variante oscura
-          700: '#0F0E16', // Variante oscura
+          100: '#5A5872',
+          200: '#3B394D',
+          DEFAULT: '#1C1A27',
+          600: '#16151F',
+          700: '#0F0E16',
         },
         green: {
-          100: '#E0F4EF', // Variante clara
-          200: '#B5E4D9', // Variante clara
-          DEFAULT: '#6ABAA4', // Color principal
-          600: '#559583', // Variante oscura
-          700: '#407062', // Variante oscura
+          100: '#E0F4EF',
+          200: '#B5E4D9',
+          DEFAULT: '#6ABAA4',
+          600: '#559583',
+          700: '#407062',
         },
+      },
+      fontFamily: {
+        'sans': ['Poppins', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      },
+      fontWeight: {
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addBase, theme }) {
+      addBase({
+        'body': { color: theme('colors.dark.DEFAULT') },
+      })
+    }
+  ],
 }
