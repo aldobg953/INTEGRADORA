77b4470c1af311abd52ce871b3cc02a708473f06
Aldo Barrera <aldobg953@gmail.com>
2024-07-30 12:16:41 -0600

    Update URLs in JavaScript files to use absolute paths

--- Integradora-1/src/main/resources/static/js/cambiarIdioma.js
+++ Integradora-1/src/main/resources/static/js/cambiarIdioma.js
@@ -2,7 +2,6 @@ function changeLang(selectElement) {
     var selectedLang = selectElement.value;
     var currentUrl = window.location.href;
     
-    // Reemplazar o añadir el parámetro lang en la URL actual
     var updatedUrl;
     if (currentUrl.includes('?')) {
         updatedUrl = currentUrl.replace(/(\?lang=)[^\&]+/, '$1' + selectedLang);
@@ -10,6 +9,5 @@ function changeLang(selectElement) {
         updatedUrl = currentUrl + '?lang=' + selectedLang;
     }
     
-    // Redirigir a la nueva URL
     window.location.href = updatedUrl;
 }
